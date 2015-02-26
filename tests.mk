b5e68fb792e591dd3da54943de646e0c0a807d07
Mickaël Wolff <mickael@lupusmic.org>
2015-02-26 11:47:17 +0100

    Tests: generic cgreen to have colour switch activated

--- tests.mk
+++ tests.mk
@@ -17,55 +17,57 @@ TESTS= \
 	   $(TESTDIR)/test_url \
 	   $(TESTDIR)/test_mysql_var \
 
+UNITTESTER=cgreen-runner --colours
+
 .PHONY: all
 
 all: $(TESTS)
 
 # Query test ###########################################################
 tests/test_query: tests/test_query.so
-	cgreen-runner $^
+	$(UNITTESTER) $^
 
 tests/test_query.so: src/query.o src/query_clause.o tests/test_query.o
 	$(CXX) -shared -Wl,-soname,$@ -o $@ $^ $(LDFLAGS) -fPIC
 
 # HTTP input request ###################################################
 tests/test_http_request: tests/test_http_request.so
-	cgreen-runner $^
+	$(UNITTESTER) $^
 
 tests/test_http_request.so: src/http_request.o tests/test_http_request.o
 	$(CXX) -shared -Wl,-soname,$@ -o $@ $^ $(LDFLAGS) -fPIC
 
 # Router tests #########################################################
 tests/test_router: tests/test_router.so
-	cgreen-runner $^
+	$(UNITTESTER) $^
 
 tests/test_router.so: src/router.o tests/test_router.o
 	$(CXX) -shared -Wl,-soname,$@ -o $@ $^ $(LDFLAGS) -fPIC
 
 # URL tests ############################################################
 tests/test_url: tests/test_url.so
-	cgreen-runner $^
+	$(UNITTESTER) $^
 
 tests/test_url.so: src/url.o tests/test_url.o
 	$(CXX) -shared -Wl,-soname,$@ -o $@ $^ $(LDFLAGS) -fPIC
 
 # MySQL Variable tests #################################################
 tests/test_mysql_var: tests/test_mysql_var.so
-	cgreen-runner $^
+	$(UNITTESTER) $^
 
 tests/test_mysql_var.so: src/mysql_var.o tests/test_mysql_var.o
 	$(CXX) -shared -Wl,-soname,$@ -o $@ $^ $(LDFLAGS) -fPIC
 
 # JSON parser tests ####################################################
 tests/test_json: tests/test_json.so
-	cgreen-runner $^
+	$(UNITTESTER) $^
 
 tests/test_json.so: src/json_parser.o src/json.o tests/test_json.o
 	$(CXX) -shared -Wl,-soname,$@ -o $@ $^ $(LDFLAGS) -fPIC
 
 # Model test ###########################################################
 tests/test_model: tests/test_model.so
-	cgreen-runner $^
+	$(UNITTESTER) $^
 
 tests/test_model.so: src/model.o src/json.o src/json_parser.o src/mysql_connection.o src/mysql_result.o src/mysql_var.o tests/test_model.o
 	$(CXX) -shared -Wl,-soname,$@ -o $@ $^ $(LDFLAGS) -fPIC
