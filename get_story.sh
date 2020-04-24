5ff651231e2eb2d6ff774b40f566ea1d1d1b9668
Mickaël Wolff <m@lupusmic.org>
2020-04-24 04:02:27 +0200

    Add quick guidelines

--- get_story.sh
+++ get_story.sh
@@ -4,6 +4,7 @@
 #curl -i -X GET -H 'Content-type: text/html' http://webpp.localhost/stories/id=1
 
 EXEC=./webpp
+export WEBPP_CONF_DIR=./conf
 export REQUEST_URI=/stories/id=1 REQUEST_METHOD=GET; $EXEC
 export REQUEST_URI=/stories/id=1 REQUEST_METHOD=GET CONTENT_TYPE=application/json; $EXEC
 export REQUEST_URI=/stories/id=1 REQUEST_METHOD=GET CONTENT_TYPE=text/html; $EXEC
